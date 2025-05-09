824549c16f9a093074ac5bc85bc71bb1be8a5c74
reina.al-masri <reina.al-masri@universite-paris-saclay.fr>
2025-05-09 13:58:25 +0200

    docstring en anglais pour cohérence du projet

--- modules/matrix.py
+++ modules/matrix.py
@@ -3,13 +3,16 @@ import random
 
 def random_int_list(n, bound):
     '''
-    return une liste de taille n contenant des entiers aléatoires entre 0 et bound
+    return a list of length n with random integers between 0 and bound (excluded)
     '''
     return [random.randrange(0, bound) for _ in range(n)]
 
 def random_int_matrix(n, bound, null_diag=True, number_generator=(lambda : random.random())):
     '''
-    return une matrice n x n avec des entiers aléatoires entre 0 et bound
+    return an n × n matrix with random integers between 0 and bound
+
+    null_diag: if True, sets the diagonal to 0
+    number_generator: function returning float in [0,1); defaults to random.random.-
     '''
     res = []
     for i in range(n):
@@ -23,7 +26,8 @@ def random_int_matrix(n, bound, null_diag=True, number_generator=(lambda : rando
 
 def random_symetric_int_matrix(n, bound, null_diag = True):
     '''
-    return une matrice symétrique n x n avec des entiers aléatoires entre 0 et bound, la diaginale est mise à zéro
+    return an n × n symmetric matrix with random integers between 0 and bound
+    the diagonal is set to 0 if null_diag is True
     '''
     res = random_int_matrix(n, bound, null_diag)
     for i in range(n):
@@ -33,7 +37,8 @@ def random_symetric_int_matrix(n, bound, null_diag = True):
 
 def random_oriented_int_matrix(n ,bound, null_diag=True):
     '''
-    return une matrice orientée n x n avec des entiers aléatoires entre 0 et bound
+    return an n × n oriented matrix with random integers between 0 and bound
+    the matrix is made asymmetric by zeroing one direction for each (i, j)
     '''
     res = random_int_matrix(n,bound,null_diag)
     for i in range(n):
@@ -46,7 +51,8 @@ def random_oriented_int_matrix(n ,bound, null_diag=True):
 
 def random_triangular_int_matrix(n ,bound, null_diag = True):
     '''
-    return une matrice triangulaire supérieure n x n avec des entiers aléatoires entre 0
+    return an n × n upper triangular matrix with random integers between 0 and bound
+    the diagonal is set to 0 if null_diag is True
     '''
     res = [[0] * n for _ in range(n)]
     for i in range(n):
